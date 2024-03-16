a3fde579b12c2851baaba8578337ee944609239e
Gabriel Roecc <gabrielroecc@gmail.com>
2024-03-16 17:56:28 -0300

    updates

--- script/main.js
+++ script/main.js
@@ -47,6 +47,20 @@ document.addEventListener("DOMContentLoaded", function () {
   }, 100);
 });
 
+const section = document.querySelector(".section_a");
+
+document.addEventListener("mousemove", (e) => {
+  const mouseY = e.clientY / window.innerHeight; // Posição vertical do mouse relativa à altura da janela
+
+  // Defina o deslocamento do background usando a posição do mouse
+  section.style.backgroundPositionX = mouseY * 700 + "%";
+});
+
+// BUTTONS BUTTONS BUTTONS BUTTONS BUTTONS BUTTONS BUTTONS BUTTONS BUTTONS BUTTONS BUTTONS BUTTONS BUTTONS
+// BUTTONS BUTTONS BUTTONS BUTTONS BUTTONS BUTTONS BUTTONS BUTTONS BUTTONS BUTTONS BUTTONS BUTTONS BUTTONS
+// BUTTONS BUTTONS BUTTONS BUTTONS BUTTONS BUTTONS BUTTONS BUTTONS BUTTONS BUTTONS BUTTONS BUTTONS BUTTONS
+// BUTTONS BUTTONS BUTTONS BUTTONS BUTTONS BUTTONS BUTTONS BUTTONS BUTTONS BUTTONS BUTTONS BUTTONS BUTTONS
+// BUTTONS BUTTONS BUTTONS BUTTONS BUTTONS BUTTONS BUTTONS BUTTONS BUTTONS BUTTONS BUTTONS BUTTONS BUTTONS
 const buttons = document.querySelectorAll(".button");
 
 buttons.forEach((button) => {
@@ -63,4 +77,83 @@ function moveButton(event) {
   const movementY = -(mouseY - rect.height / 2) / 10;
 
   this.style.transform = `translate(${movementX}px, ${movementY}px)`;
+}
+
+const buttons2 = document.querySelectorAll(".button_second");
+
+buttons2.forEach((button2) => {
+  button2.addEventListener("mousemove", moveButton2);
+});
+
+function moveButton2(event) {
+  const rect = this.getBoundingClientRect();
+  const mouseX = event.clientX - rect.left;
+  const mouseY = event.clientY - rect.top;
+
+  // Ajuste os valores conforme desejado para controlar a intensidade do movimento
+  const movementX = -(mouseX - rect.width / 2) / 10;
+  const movementY = -(mouseY - rect.height / 2) / 10;
+
+  this.style.transform = `translate(${movementX}px, ${movementY}px)`;
+}
+
+// SECOND SECTION SECOND SECTION SECOND SECTION SECOND SECTION SECOND SECTION SECOND SECTION SECOND SECTION
+// SECOND SECTION SECOND SECTION SECOND SECTION SECOND SECTION SECOND SECTION SECOND SECTION SECOND SECTION
+// SECOND SECTION SECOND SECTION SECOND SECTION SECOND SECTION SECOND SECTION SECOND SECTION SECOND SECTION
+// SECOND SECTION SECOND SECTION SECOND SECTION SECOND SECTION SECOND SECTION SECOND SECTION SECOND SECTION
+
+var secondSection = document.querySelector(".second_section");
+var animationStarted = false;
+
+window.addEventListener("scroll", function () {
+  var positionFromTop = secondSection.getBoundingClientRect().top;
+  var screenHeight = window.innerHeight;
+
+  // Defina uma margem superior para quando você quer que a transição comece
+  var triggerPoint = 0.1 * screenHeight;
+
+  if (positionFromTop < triggerPoint) {
+    if (!animationStarted) {
+      animateOpacity(1); // Iniciar animação para aumentar a opacidade
+      animationStarted = true;
+    }
+  } else {
+    if (animationStarted) {
+      animateOpacity(0); // Iniciar animação para diminuir a opacidade
+      animationStarted = false;
+    }
+  }
+});
+
+function animateOpacity(targetOpacity) {
+  var opacity = parseFloat(secondSection.style.opacity) || 0;
+  var startTime = null;
+  var duration = 1000;
+
+  function animate(currentTime) {
+    if (!startTime) startTime = currentTime;
+    var elapsedTime = currentTime - startTime;
+
+    if (elapsedTime < duration) {
+      // Duração da animação em milissegundos
+      opacity = opacity + (targetOpacity - opacity) * (elapsedTime / duration); // Interpolação linear
+      secondSection.style.opacity = opacity;
+      requestAnimationFrame(animate);
+    } else {
+      secondSection.style.opacity = targetOpacity;
+      // Após a conclusão da animação de opacidade, inicie a animação de fade nas imagens das cartas
+      fadeInCards();
+    }
+  }
+
+  requestAnimationFrame(animate);
+}
+
+function fadeInCards() {
+  var cards = document.querySelectorAll(".card");
+  cards.forEach(function (card, index) {
+    setTimeout(function () {
+      card.classList.add("fade-in");
+    }, index * 500); // Atraso de 200ms entre cada imagem (ajustado para um intervalo menor)
+  });
 }
