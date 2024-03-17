6250e024098a8a7cc0b47af398542ae85a36d5b5
Gabriel Roecc <gabrielroecc@gmail.com>
2024-03-17 20:32:52 -0300

    update

--- script/main.js
+++ script/main.js
@@ -162,23 +162,39 @@ function fadeInCards() {
 }
 
 // SECTION B
-const animationDelay = 100; // 1 segundo
-const animationDuration = 2000; // 0.5 segundo
+const animationDelay = 1000; // 1 segundo
+const animationDuration = 500; // 0.5 segundo
 
 window.addEventListener("scroll", function () {
-  const element = document.querySelector(".about_us_h1");
-  const positionFromTop = element.getBoundingClientRect().top;
+  const titleElement = document.querySelector(".about_us_h1");
+  const paragraphElement = document.querySelector(".about_us");
+  const buttonElement = document.querySelector(".about_us_button");
+  const titlePositionFromTop = titleElement.getBoundingClientRect().top;
+  const paragraphPositionFromTop = paragraphElement.getBoundingClientRect().top;
+  const buttonPositionFromTop = buttonElement.getBoundingClientRect().top;
   const screenHeight = window.innerHeight;
-  const triggerPoint = 0.2 * screenHeight;
+  const triggerPoint = 0.1 * screenHeight;
 
-  if (positionFromTop - screenHeight + triggerPoint <= 0) {
+  if (titlePositionFromTop - screenHeight + triggerPoint <= 0) {
     setTimeout(function () {
-      element.style.transitionDuration = `${animationDuration}ms`;
-      animateSlide(element, 0); // Animar para dentro
+      titleElement.style.transitionDuration = `${animationDuration}ms`;
+      animateSlide(titleElement, 0); // Animar título para dentro
+      setTimeout(function () {
+        paragraphElement.style.transitionDuration = `${animationDuration}ms`;
+        animateSlide(paragraphElement, 0); // Animar parágrafo para dentro após a animação do título
+        setTimeout(function () {
+          buttonElement.style.transitionDuration = `${animationDuration}ms`;
+          animateSlide(buttonElement, 0); // Animar botão para dentro após a animação do parágrafo
+        }, animationDuration);
+      }, animationDuration);
     }, animationDelay);
   } else {
-    element.style.transitionDuration = `${animationDuration}ms`;
-    animateSlide(element, -100); // Animar para fora
+    titleElement.style.transitionDuration = `${animationDuration}ms`;
+    animateSlide(titleElement, -100); // Animar título para fora
+    paragraphElement.style.transitionDuration = `${animationDuration}ms`;
+    animateSlide(paragraphElement, -100); // Animar parágrafo para fora
+    buttonElement.style.transitionDuration = `${animationDuration}ms`;
+    animateSlide(buttonElement, -100); // Animar botão para fora
   }
 });
 
@@ -186,3 +202,68 @@ function animateSlide(element, offsetX) {
   element.style.opacity = offsetX === 0 ? 1 : 0; // Se offsetX é 0, definir a opacidade como 1, caso contrário, definir como 0
   element.style.transform = `translateX(${offsetX}%)`; // Usar offsetX para definir a posição
 }
+
+const sectionB = document.querySelector(".section_b");
+
+sectionB.addEventListener("mousemove", function (event) {
+  const mouseX = event.clientX;
+  const mouseY = event.clientY;
+
+  const offsetX = (mouseX - window.innerWidth / 2) / 30; // Ajuste a sensibilidade do movimento horizontal
+  const offsetY = (mouseY - window.innerHeight / 2) / 30; // Ajuste a sensibilidade do movimento vertical
+
+  sectionB.style.backgroundPosition = `${50 + offsetX}% ${50 + offsetY}%`;
+});
+
+// SECTION C
+const animationDelayC = 1000; // 1 segundo
+const animationDurationC = 500; // 0.5 segundo
+const fadeInDuration = 500; // Duração do fade-in da div .galery_wrapper
+const delayAfterTitleAnimation = 100; // 1 segundo de atraso após a animação do título
+
+window.addEventListener("scroll", function () {
+  const titleElementC = document.querySelector(".section_c h1");
+  const titlePositionFromTopC = titleElementC.getBoundingClientRect().top;
+  const screenHeight = window.innerHeight;
+  const triggerPoint = 0.1 * screenHeight;
+
+  if (titlePositionFromTopC - screenHeight + triggerPoint <= 0) {
+    setTimeout(function () {
+      titleElementC.style.transitionDuration = `${animationDurationC}ms`;
+      animateSlide(titleElementC, 0); // Animar título para dentro
+      setTimeout(function () {
+        // Seleciona a div .galery_wrapper
+        const galeryWrapper = document.querySelector(".galery_wrapper");
+
+        // Define a transição para a div .galery_wrapper
+        galeryWrapper.style.transition = `opacity ${fadeInDuration}ms`;
+        galeryWrapper.style.opacity = 1;
+      }, delayAfterTitleAnimation);
+    }, animationDelayC);
+  } else {
+    titleElementC.style.transitionDuration = `${animationDurationC}ms`;
+    animateSlide(titleElementC, -100); // Animar título para fora
+  }
+});
+
+function animateSlide(element, offsetX) {
+  element.style.opacity = offsetX === 0 ? 1 : 0; // Se offsetX é 0, definir a opacidade como 1, caso contrário, definir como 0
+  element.style.transform = `translateX(${offsetX}%)`; // Usar offsetX para definir a posição
+}
+
+// Inicializa a div .galery_wrapper com opacidade 0
+document.addEventListener("DOMContentLoaded", function () {
+  const galeryWrapper = document.querySelector(".galery_wrapper");
+  galeryWrapper.style.opacity = 0;
+});
+
+// SECTION D
+const sectionD = document.querySelector(".section_d");
+
+sectionD.addEventListener("mousemove", function (event) {
+  const mouseY = event.clientY;
+
+  const offsetY = (mouseY - window.innerWidth / 2) / 30; // Ajuste a sensibilidade do movimento horizontal
+
+  sectionD.style.backgroundPosition = `${100 + offsetY}% ${190 + offsetY}%`;
+});
